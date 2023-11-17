999a936bd939e28092c091d693cb43321cc677be
khubtipopon <145144946+khubtipopon@users.noreply.github.com>
2023-11-17 19:49:04 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function flipCoin() {
+    
+    
+var coin = document.getElementById('coin');
+    
+    
+var result = Math.random() < 0.5 ? 'орел' : 'решка';
+    coin.
+    
+innerText = result;
+}
